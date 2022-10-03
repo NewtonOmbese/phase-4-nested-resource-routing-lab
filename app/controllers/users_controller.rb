f899c46ff63cbdbbb57038405e80b7821e1953d4
Newton Ombese <newtonombese1@gmail.com>
2022-10-03 09:09:33 +0300

    Practicing and revisiting will get me there more lethal and impossible to beat

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     user = User.find_by(id: params[:id])
-    render json: user, include: :items
+    render json: user.to_json(except: [:created_at, :updated_at], include: :items)
   end
 
 end
